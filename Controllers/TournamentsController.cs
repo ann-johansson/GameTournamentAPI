d8d2fb09534d8afba729d584c906028b60828ac6
Ann Johansson <ann.johansson.work@gmail.com>
2026-02-18 13:02:52 +0100

    Created a new object in models called Tournament. Created a service class for Tournament. Created a controller class for Tournament.

---
+++ Controllers/TournamentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameTournamentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TournamentsController : ControllerBase
+    {
+    }
+}
---
+++ Services/TournamentsService.cs
@@ -0,0 +1,12 @@
+﻿using GameTournamentAPI.Models;
+
+namespace GameTournamentAPI.Services
+{
+    public class TournamentsService
+    {
+        private readonly List<Tournament> _tournaments = new List<Tournament>();
+        private int _nextId = 1;
+
+
+    }
+}
