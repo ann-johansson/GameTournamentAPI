5051090a9e96d734b9b5ec295533fc9cd692e150
Ann Johansson <ann.johansson.work@gmail.com>
2026-02-20 19:59:37 +0100

    Fixed DTOs: adjust Id and MaxPlayers properties.
    Added Id to TournamentResponseDTO and removed MaxPlayers from it.
    In TournamentUpdateDTO, removed Id and added MaxPlayers.

--- DTOs/TournamentResponseDTO.cs
+++ DTOs/TournamentResponseDTO.cs
@@ -2,9 +2,9 @@
 {
     public class TournamentResponseDTO
     {
+        public int Id { get; set; }
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
-        public int MaxPlayers { get; set; }
         public DateTime Date { get; set; }
     }
 }
--- DTOs/TournamentUpdateDTO.cs
+++ DTOs/TournamentUpdateDTO.cs
@@ -2,9 +2,9 @@
 {
     public class TournamentUpdateDTO
     {
-        public int Id { get; set; }
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
+        public int MaxPlayers { get; set; }
         public DateTime Date { get; set; }
     }
 }
